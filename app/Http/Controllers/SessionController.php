10527f2dd115a7d52f6010a2d6d728972e559ee3
LLL <1401582905@qq.com>
2020-04-27 05:59:20 +0000

    创建会话

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '登录成功');
+            return redirect()->route('user.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的账号密码不匹配');
+            return redirect()->back()->withInput();
+        }
+
+        return;
+    }
+}
