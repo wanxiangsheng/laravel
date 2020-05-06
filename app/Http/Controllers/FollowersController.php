bf93a17152f5218661cd467b25020b4ab4fbbf52
LLL <1401582905@qq.com>
2020-05-06 03:13:22 +0000

    关注用户动态流

--- app/Http/Controllers/FollowersController.php
+++ app/Http/Controllers/FollowersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowersController extends Controller
@@ -10,7 +11,7 @@ class FollowersController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function store(User $user)
     {
         $this->authorize('follow', $user);
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,10 +58,6 @@ class User extends Authenticatable
         $this->hasMany(Status::class);
     }
 
-    public function feed(){
-        return $this->statuses()->orderBy('created_at','desc');
-    }
-
     public function followers()
     {
         return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
@@ -91,5 +87,14 @@ class User extends Authenticatable
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
+    }
+
+    public function feed()
+    {
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
     }
 }
